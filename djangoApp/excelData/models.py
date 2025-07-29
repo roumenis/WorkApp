192bc48c11a8aa80fe8a37b5386d4a9da995d40e
roumenis <r.urban.it@gmail.com>
2025-07-29 14:08:29 +0200

    models "ExcelData" created

--- djangoApp/excelData/models.py
+++ djangoApp/excelData/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ExcelData(models.Model):
+    kurz = models.CharField(max_length=5) # 12/810
+    linka = models.DecimalField(max_digits=5, decimal_places=2) # 13 ()
+    prijezd = models.CharField(max_length=5) # 10:00
+    odjezd = models.CharField(max_length=5) # 10:05
+    kolej = models.CharField(max_length=1) # 0,1,2...
+    
+    
